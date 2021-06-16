bb97b8aceb0e11a7311dbf2f442062b8fcab7fee
Wendell Silva <wendellxj@hotmail.com>
2021-06-16 19:07:08 -0300

    aula-05 aula 94

--- 06-webserver/app_aula94.js
+++ 06-webserver/app_aula94.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
 
-const port = 8080;
+const express = require('express');
+var hbs = require('hbs');
+///configurar variaveis de entorno para ler as chaves ... o arquivo TXT foi alterado para .env  para ficar um path do caminho
+require('dotenv').config()
+
+const app = express();
+
+///SUBSTITUDI PELO DOTENV
+///const port = 8080;
+const port = process.env.PORT;
  
 //TODO: require ('hb')-->handlebars
 app.set('view engine', 'hbs');
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 ///Servidor contenido estático... especificar o path da pasta public com o arquivo html
 ///equivale a rota '/'
@@ -21,13 +28,45 @@ app.get('/',  (req, res) => {
   });
 });
 
+//Usar handlebars para renderizar as paginas
+
+app.get('/home',  (req, res) => { 
+  res.render('home', { 
+    nombre: 'Wendel Silva',
+    titulo: 'Curso de Node'
+  });
+});
+
+
+//Usar handlebars para renderizar as paginas
+
+app.get('/elements',  (req, res) => { 
+  res.render('elements', { 
+    nombre: 'Wendel Silva',
+    titulo: 'Curso de Node'
+  });
+});
+
+
+//Usar handlebars para renderizar as paginas
+
+app.get('/generic',  (req, res) => { 
+  res.render('generic', { 
+    nombre: 'Wendel Silva',
+    titulo: 'Curso de Node'
+  });
+});
+
+
+
+
 ///colocar URL sem a extensão do arquivo ex. index.html
 
 /*
 app.get('/home',  (req, res) => { 
   res.sendFile( __dirname + '/public/templates/index.html')
 })
-*/
+
 
 ///colocar URL sem a extensão do arquivo ex. index.html
 
@@ -41,7 +80,7 @@ app.get('/generic',  (req, res) => {
 app.get('/elements',  (req, res) => { 
   res.sendFile( __dirname + '/public/templates/elements.html')
 })
-
+*/
 
 /*
 ///Definir a rota hola mundo ex 3
