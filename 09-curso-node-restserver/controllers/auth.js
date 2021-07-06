37556269137d3596e86aa9253b358ab7da1cd6c8
Wendell Nascimento <wendellxj@hotmail.com>
2021-07-06 15:03:10 -0300

    aula 9 primeiro commit 06072021

--- 09-curso-node-restserver/controllers/auth.js
+++ 09-curso-node-restserver/controllers/auth.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario');
 
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -53,7 +54,83 @@ const login = async(req, res = response) => {
 }
 
 
+const googleSignin = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    
+    
+    try {  
+
+    //const googleUser = await googleVerify ( id_token ); ///chama a função do google de helpers 
+    
+    //Desestrutura a variavel enviada
+    const { correo, nombre, img } = await googleVerify ( id_token ); 
+
+     // console.log ( { correo, nombre, img } );
+    //gerar referencia para ver se o email existe no banco o email
+    
+    let usuario = await Usuario.findOne({ correo });
+
+     if ( !usuario ) {
+        //se o usuario não existe tem que criar
+         const data = {
+                nombre,
+                correo,
+                password: `:P`,///nÃO É NECESSARIO CADATRAR SENHA,
+                img,
+                google: true
+           };
+           //Gravrar o usuario no banco de dados caso não exista
+
+        //console.log( data );
+        usuario = new Usuario( data );
+        await usuario.save();
+
+        }
+
+        /// se el usuario existe en DB
+        if ( !usuario.estado ) { //usuario estado diferente de verdadeiro (booleano) {
+            return res.status(401).json({
+
+                msg: 'Hable con el administrador , usuario bloqueado'
+
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT( usuario.id )
+
+
+   /// console.log ( googleUser );
+
+
+    res.json({
+        usuario,
+        token
+        //msg: 'Todo ok! google signin',
+        //id_token
+    });
+
+
+} catch (error) {
+    res.status(400).json({
+
+        msg: 'Token de google no es válido'
+    })
+
+
+
+}
+
+}
+
+
+
+
+
 
 module.exports = {
-    login
+    login,
+    googleSignin
 }
